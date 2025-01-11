424e84c97a4a349ea8535768b1e35a36a58bea1b
unumest <unumest.com@gmail.com>
2025-01-11 12:46:20 +0900

    250111

--- src/pages/LogoPage.tsx
+++ src/pages/LogoPage.tsx
@@ -2,6 +2,7 @@ import styles from '../styles/LogoPage.module.css';
 import Logo from '../components/Logo';
 import {useNavigate} from 'react-router-dom';
 import PixelWriting from '../components/PixelWriting';
+import {DEFAULT_FONT_SIZE} from '../configs/configs';
 
 export default function LogoPage() {
     const navigate = useNavigate();
@@ -15,7 +16,7 @@ export default function LogoPage() {
                 style={{cursor: 'pointer'}}
             >
                 {/* <Logo multi={8} /> */}
-                <PixelWriting str={'UnumEst'} fontProps={{size: 10}} />
+                <PixelWriting str={'UnumEst'} fontProps={{size: DEFAULT_FONT_SIZE}} />
             </div>
         </div>
     );
